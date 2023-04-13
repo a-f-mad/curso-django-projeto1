31f00a5a4a54f1987e77383de94d8f1df678ee51
a-f-mad <rek4zbie@duck.com>
2023-04-13 16:15:39 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contact(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def about(request):
-    return HttpResponse('Sobre')
